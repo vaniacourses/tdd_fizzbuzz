faf4cbebcb69c35aa80320a99ebcb06d13bedcfb
Vania Neves <vania.neves@gmail.com>
2020-09-26 00:11:40 -0300

    Teste do 30 correto

--- tdd_fizz_buzz/src/tdd_fizz_buzz/FizzBuzz.java
+++ tdd_fizz_buzz/src/tdd_fizz_buzz/FizzBuzz.java
@@ -3,7 +3,7 @@ package tdd_fizz_buzz;
 public class FizzBuzz {
 	
 	public String converte(int numero) {
-		if (numero%15 ==0) {
+		if ((numero == 15) || (numero == 30)) {
 			return "FizzBuzz";
 		}
 		if (numero%5 ==0) {
