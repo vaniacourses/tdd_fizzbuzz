552c20130535150a3e14cb20eb8407a396032633
Vania Neves <vania.neves@gmail.com>
2020-09-25 23:50:55 -0300

    Teste do 1

---
+++ tdd_fizz_buzz/src/tdd_fizz_buzz/FizzBuzz.java
@@ -0,0 +1,9 @@
+package tdd_fizz_buzz;
+
+public class FizzBuzz {
+	
+	public String converte(int numero) {
+		return "1";
+	}
+
+}
---
+++ tdd_fizz_buzz/test/tdd_fizz_buzz/FizzBuzzTest.java
@@ -0,0 +1,17 @@
+package tdd_fizz_buzz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzTest {
+	
+	@Test
+	public void testNumerosMenores() {
+		
+		FizzBuzz fb = new FizzBuzz();
+		Assertions.assertEquals("1", fb.converte(1));
+		Assertions.assertEquals("2", fb.converte(2));
+		
+	}
+
+}
